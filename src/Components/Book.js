9bad6cc2ec455cadd816acef2935c3c467edb226
ShehabGamal <shehab.gamal333@gmail.com>
2021-10-23 14:15:08 +0200

    fix: Handled Bug Related To missing Thumbnails & Authors

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 
 const Book = ({book,shelfChanger})=>{
+    
+  const Thumbnail = book.imageLinks?book.imageLinks.thumbnail:"";
+  const Author = book.authors?book.authors:"";
     return(
         <div className="book">
             <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${Thumbnail}")` }}></div>
               <div className="book-shelf-changer">
                 <select onChange={(e)=>shelfChanger(book,e.target.value)} value={book.shelf}>
                   <option value="move" disabled>Move to...</option>
@@ -17,7 +20,7 @@ const Book = ({book,shelfChanger})=>{
               </div>
             </div>
             <div className="book-title">{book.title}</div>
-            <div className="book-authors">{book.authors}</div>
+            <div className="book-authors">{Author}</div>
           </div>
     );
 
